8a7242a67a80c56cdd243479eda17285c24b805d
MAKIROR <gzanan@gmail.com>
2022-10-10 09:05:01 -0400

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for input in io::stdin().bytes() {
+        let value = input.unwrap() as char;
+        
+        if value.is_control() {
+            
+        } else {
+            
+        }
+    }
 }
---
+++ target/debug/ROR_Text.d
@@ -0,0 +1,1 @@
+/home/makiror/ROR_Text/target/debug/ROR_Text: /home/makiror/ROR_Text/src/main.rs
