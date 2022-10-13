62175ceb4b3491292df06af929bc2f243dad9d0e
MAKIROR <gzanan@gmail.com>
2022-10-13 09:04:10 -0400

    Update

--- target/debug/ROR_Text.d
+++
@@ -1,1 +0,0 @@
-/home/makiror/ROR_Text/target/debug/ROR_Text: /home/makiror/ROR_Text/library/inputlib/src/main.rs /home/makiror/ROR_Text/src/main.rs
--- target/debug/deps/ROR_Text-cf7293196de08840.d
+++
@@ -1,5 +0,0 @@
-/home/makiror/ROR_Text/target/debug/deps/ROR_Text-cf7293196de08840: src/main.rs
-
-/home/makiror/ROR_Text/target/debug/deps/ROR_Text-cf7293196de08840.d: src/main.rs
-
-src/main.rs:
