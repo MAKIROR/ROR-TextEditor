12bd555657ae63c4fe83c9bbb1a2cdc5f9de5f6b
MAKIROR <gzanan@gmail.com>
2022-10-15 07:03:38 -0400

    Update

--- src/modules/document.rs
+++ src/modules/document.rs
@@ -1,4 +1,5 @@
 use crate::Row;
+use std::fs;
 
 #[derive(Default)]
 pub struct Document {
@@ -6,12 +7,21 @@ pub struct Document {
 }
 
 impl Document {
-    pub fn open() -> Self {
+    pub fn open(filename: &str) -> Result<Self,std::io::Error> {
+        let contents = fs::read_to_string(filename)?;
         let mut rows = Vec::new();
-        rows.push(Row::from("Hello, World!"));
-        Self { rows }
+        for value in contents.lines() {
+            rows.push(Row::from(value));
+        }
+        Ok(Self{rows})
     }
     pub fn row(&self, index: usize) -> Option<&Row> {
         self.rows.get(index)
+    }
+    pub fn is_empty(&self) -> bool {
+        self.rows.is_empty()
+    }
+    pub fn len(&self) -> usize {
+        self.rows.len()
     }
 }
--- src/modules/rorlib.rs
+++ src/modules/rorlib.rs
@@ -2,12 +2,14 @@ use crate::Terminal;
 use termion::event::Key;
 use crate::Document;
 use crate::Row;
+use std::env;
 
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
     document: Document,
+    offset: Position,
 }
 
 #[derive(Default)]
@@ -19,6 +21,24 @@ pub struct Position {
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 impl Editor {
+    pub fn default() -> Self {
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
+
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+            document,
+            cursor_position: Position::default(),
+            offset: Position::default(),
+        }
+    }
+
     pub fn run(&mut self) {
         loop {
             if let Err(error) = self.refresh_screen() {
@@ -32,15 +52,6 @@ impl Editor {
             if let Err(error) = self.match_key() {
                 program_die(error);
             }
-        }
-    }
-
-    pub fn default() -> Self {
-        Self {
-            should_quit: false,
-            terminal: Terminal::default().expect("Failed to initialize terminal"),
-            cursor_position: Position::default(),
-            document: Document::open(),
         }
     }
 
@@ -59,13 +70,14 @@ impl Editor {
             => self.move_cursor(key),
             _ => (),
         }
+        self.scroll();
         Ok(())
     }
 
     fn move_cursor(&mut self, key: Key) {
         let Position { mut y, mut x } = self.cursor_position;
         let size = self.terminal.size();
-        let height = size.height.saturating_sub(1) as usize;
+        let height = self.document.len();
         let width = size.width.saturating_sub(1) as usize;
         match key {
             Key::Up => y = y.saturating_sub(1),
@@ -110,7 +122,7 @@ impl Editor {
             Terminal::clear_current_line();
             if let Some(row) = self.document.row(terminal_row as usize) {
                 self.draw_row(row);
-            } else if terminal_row == height / 3 {
+            } else if terminal_row == height / 3 && self.document.is_empty() {
                 self.draw_welcome_message();
             } else {
                 println!("~\r");
@@ -119,11 +131,13 @@ impl Editor {
     }
 
     pub fn draw_row(&self, row: &Row) {
-        let start = 0;
-        let end = self.terminal.size().width as usize;
+        let width = self.terminal.size().width as usize;
+        let start = self.offset.x;
+        let end = self.offset.x + width;
         let row = row.render(start, end);
         println!("{}\r", row)
     }
+    
 
     fn draw_welcome_message(&self) {
         let mut welcome_message = format!("ROR Text Editor -- version {}", VERSION);
@@ -136,7 +150,22 @@ impl Editor {
         println!("{}\r", welcome_message);
     }
 
-
+    fn scroll(&mut self) {
+        let Position { x, y } = self.cursor_position;
+        let width = self.terminal.size().width as usize;
+        let height = self.terminal.size().height as usize;
+        let mut offset = &mut self.offset;
+        if y < offset.y {
+            offset.y = y;
+        } else if y >= offset.y.saturating_add(height) {
+            offset.y = y.saturating_sub(height).saturating_add(1);
+        }
+        if x < offset.x {
+            offset.x = x;
+        } else if x >= offset.x.saturating_add(width) {
+            offset.x = x.saturating_sub(width).saturating_add(1);
+        }
+    }
     
 }
 
