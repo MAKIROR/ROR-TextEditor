002299e5eb1e7d75e9ff9bde51b99f1d709a1fc1
MAKIROR <gzanan@gmail.com>
2022-10-15 05:31:03 -0400

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 #![warn(clippy::all, clippy::pedantic)]
 
+#[path = "modules/rorlib.rs"]
 mod rorlib;
+
+#[path = "modules/terminal.rs"]
 mod terminal;
+
+#[path = "modules/document.rs"]
+mod document;
+
+#[path = "modules/row.rs"]
+mod row;
 
 use std::io::{self, stdout};
 use rorlib::Editor;
 pub use terminal::Terminal;
 pub use rorlib::Position;
+pub use document::Document;
+pub use row::Row;
 
 fn main() {
     Editor::default().run();
