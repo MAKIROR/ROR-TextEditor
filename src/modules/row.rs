81c8a285f8448f4dffa518995d5105aed3c943e7
MAKIROR <gzanan@gmail.com>
2022-10-16 08:13:26 -0400

    Update

--- src/modules/row.rs
+++ src/modules/row.rs
@@ -1,14 +1,19 @@
 use std::cmp;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub struct Row {
     string: String,
+    len: usize,
 }
 
 impl From<&str> for Row {
     fn from(slice: &str) -> Self {
-        Self {
+        let mut row = Self {
             string: String::from(slice),
-        }
+            len: 0,
+        };
+        row.update_len();
+        row
     }
 }
 
@@ -16,12 +21,23 @@ impl Row {
     pub fn render(&self, start: usize, end: usize) -> String {
         let end = cmp::min(end, self.string.len());
         let start = cmp::min(start, end);
-        self.string.get(start..end).unwrap_or_default().to_string()
+        let mut result = String::new();
+        for grapheme in self.string[..]
+            .graphemes(true)
+            .skip(start)
+            .take(end - start)
+        {
+            result.push_str(grapheme);
+        }
+        result
     }
     pub fn len(&self) -> usize {
-        self.string.len()
+        self.len
     }
     pub fn is_empty(&self) -> bool {
-        self.string.is_empty()
+        self.len == 0
+    }
+    fn update_len(&mut self) {
+        self.len = self.string[..].graphemes(true).count();
     }
 }
