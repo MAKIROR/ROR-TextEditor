a5169fbf99e0a818ba7f5d3925cad0ba37cde3e2
MAKIROR <87136269+MAKIROR@users.noreply.github.com>
2022-10-19 23:06:55 +0800

    Delete target directory

--- target/debug/ROR_Text.d
+++
@@ -1,1 +0,0 @@
-/home/makiror/ROR_Text/target/debug/ROR_Text: /home/makiror/ROR_Text/src/main.rs /home/makiror/ROR_Text/src/modules/document.rs /home/makiror/ROR_Text/src/modules/rorlib.rs /home/makiror/ROR_Text/src/modules/row.rs /home/makiror/ROR_Text/src/modules/terminal.rs
