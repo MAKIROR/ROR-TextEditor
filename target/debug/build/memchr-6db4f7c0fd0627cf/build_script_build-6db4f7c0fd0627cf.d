71920b29550a55faf50aa5720d33456ae365fe80
MAKIROR <gzanan@gmail.com>
2022-10-25 11:09:38 -0400

    Push RORText v0.1.0

--- target/debug/build/memchr-6db4f7c0fd0627cf/build_script_build-6db4f7c0fd0627cf.d
+++
@@ -1,5 +0,0 @@
-/home/makiror/ROR_Text/main/target/debug/build/memchr-6db4f7c0fd0627cf/build_script_build-6db4f7c0fd0627cf: /root/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs
-
-/home/makiror/ROR_Text/main/target/debug/build/memchr-6db4f7c0fd0627cf/build_script_build-6db4f7c0fd0627cf.d: /root/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs
-
-/root/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs:
