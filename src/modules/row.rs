237f0e509f21bb6d5069a44f05840faaafa21172
MAKIROR <gzanan@gmail.com>
2022-12-12 23:10:35 +0800

    Update 2022-12-12

--- src/modules/row.rs
+++ src/modules/row.rs
@@ -31,42 +31,32 @@ impl Row {
         let start = cmp::min(start, end);
         let mut result = String::new();
         let mut current_highlighting = &highlighting::Type::None;
+        #[allow(clippy::integer_arithmetic)]
         for (index, grapheme) in self.string[..]
             .graphemes(true)
             .enumerate()
             .skip(start)
             .take(end - start)
         {
-            if let Some(c) = grapheme.chars().next() {     
-                let highlighting_type = self            
-                    .highlighting            
-                    .get(index)            
-                    .unwrap_or(&highlighting::Type::None);            
-                    if highlighting_type != current_highlighting {            
-                        current_highlighting = highlighting_type;            
-                        let start_highlight =            
-                        format!("{}", termion::color::Fg(highlighting_type.to_color()));            
-                        result.push_str(&start_highlight[..]);            
-                    }
-                if c == '\t' {            
-                    result.push_str(" ");            
-                } else if c.is_ascii_digit() {            
-                    result.push_str(            
-                        &format!(            
-                            "{}{}{}",            
-                            termion::color::Fg(color::Rgb(220, 163, 163)),            
-                            c,            
-                            color::Fg(color::Reset)            
-                        )[..],            
-                    );            
-                } else {            
-                    result.push(c);            
+            if let Some(c) = grapheme.chars().next() {
+                let highlighting_type = self
+                    .highlighting
+                    .get(index)
+                    .unwrap_or(&highlighting::Type::None);
+                if highlighting_type != current_highlighting {
+                    current_highlighting = highlighting_type;
+                    let start_highlight =
+                        format!("{}", termion::color::Fg(highlighting_type.to_color()));
+                    result.push_str(&start_highlight[..]);
                 }
-                let end_highlight = format!("{}", termion::color::Fg(color::Reset));            
-                result.push_str(&end_highlight[..]);
+                if c == '\t' {
+                    result.push_str(" ");
+                } else {
+                    result.push(c);
+                }
             }
         }
-        let end_highlight = format!("{}", termion::color::Fg(color::Reset));            
+        let end_highlight = format!("{}", termion::color::Fg(color::Reset));
         result.push_str(&end_highlight[..]);
         result
     }
